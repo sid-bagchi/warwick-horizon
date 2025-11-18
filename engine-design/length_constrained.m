f39c8384dab25ade7897c623e51605148b80a261
Siddharth Bagchi <sunny@Sunnys-MacBook-Air.local>
2025-11-18 15:47:57 +0000

    Updated fuel_constrained.m script to output a surface showing O/F, Burn Time and Fuel Grain Length relationship

--- engine-design/length_constrained.m
+++ engine-design/length_constrained.m
@@ -1,19 +1,38 @@
 % ASSUME CENTRE-PERFORATED FUEL GRAIN %
 
 n = 0.5; % regression rate exponent, empirical, Ch.6, pp. 110
-PDR = 2:0.01:3; % port dia ratio Df/Di, Ch. 7, pp. 134
+PDR = 2:0.01:3.5; % port dia ratio Df/Di, Ch. 7, pp. 134
 D_f = 60e-3; % final fuel grain port dia, m
 D_i = D_f./PDR; % initial fuel grain port dia, m
-L_f = 100e-3; % fuel length, m
-m_f = 0; % total fuel mass, kg
+L_f = (50:1:200)*1e-3; % fuel length, m
+m_f = zeros(100,100); % total fuel mass, kg
 p_f = 900; % fuel density, kg m-3
-m_o = 0.5:0.02:2.5; % total oxidiser mass, kg - GUESS
+m_o = 1.5; % total oxidiser mass, kg - GUESS
 a_o = 1.55E-04; %regression rate coefficient (only in terms of oxidiser mass flux), empirical, Ch.6, pp. 110
 
-m_f = 1/4 * (L_f * (pi * p_f * (D_f^2 - D_i.^2)));
+for i = 1:length(PDR)  % Loop over each initial diameter D_i(i)
+    for j = 1:length(PDR) % Loop over each fuel length L_f(j)
+        m_f(i, j) = 1/4 * (L_f(j) * (pi * p_f * (D_f^2 - D_i(i)^2)));
+        
+    end
+end
 
-t_b = (((D_f^(2*n+1) - D_i.^(2*n+1)) .* pi^n) ./ (a_o * (2*n + 1) * 2^(2*n + 1) * m_o .^ n)) .^ (1 / (1-n)); % Check! Unrealistic values
+%m_f = 1/4 * (L_f .* (pi * p_f * (D_f^2 - D_i.^2)));
+
+t_b = (((D_f^(2*n+1) - D_i.^(2*n+1)) .* pi^n) ./ (a_o * (2*n + 1) * 2^(2*n + 1) * m_o .^ n)) .^ (1 / (1-n));
  
 
-plot(m_o./m_f, t_b);
-xlabel('O/F for N2O and Paraffin'); ylabel('burn time');
+s = surf(L_f, t_b, m_o./m_f, 'FaceAlpha',0.5);
+
+min_OF = 6.7;
+max_OF = 9.1;
+
+clim([min_OF max_OF]);
+cmap = parula(256); 
+
+cmap(1, :) = [1 0 0]; 
+cmap(end, :) = [1 0 0]; 
+
+colormap(cmap);
+
+xlabel('Fuel Grain Length / m'); ylabel('Burn Time / s'); zlabel('O/F Ratio'); colorbar; s.EdgeColor = 'none';
